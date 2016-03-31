ba542f54ddd565fd697781ae8229e417ba5580c9
Lee Briggs <lee@leebriggs.co.uk>
2016-03-31 18:01:30 +0100

    Refactor into modules for testing

--- lib/facter/tags.rb
+++ lib/facter/tags.rb
@@ -1,47 +1,51 @@
 # Creates a structured fact, tags, from all the JSON files in
 # the directory /etc/tags
-
-def valid_json?(json)
-  begin
-    JSON.parse(json)
-    return true
-  rescue JSON::ParserError => e
-    return false
-  end
-end
-
-# Read all json files from directory
-def tag_files
-  return Dir['/etc/tags/*.json']
-end
-
-def tags
-  # loop through through all those files
-  values = tag_files.map do |f|
-    file = File.read(f)
-    # Quick check for valid JSON
-    if valid_json?(file)
-      JSON.parse(file)
+require 'pp'
+module Tags
+  def self.valid_json?(json)
+    begin
+      JSON.parse(json)
+      return true
+    rescue JSON::ParserError => e
+      return false
     end
   end
-  return values
-end
 
-# Flatten all the values
-tags.flatten!
-
-# Remove all nil values
-tags.compact!
-
-# Remove all duplicate values
-tags.uniq!
-
-# Old method left here for reference
-#tags = Dir['/etc/tags/*.json'].map { |f| JSON.parse File.read(f) }.flatten
-
-Facter.add(:tags) do
-  confine :kernel => 'Linux'
-  setcode do
-    tags unless tags.empty?
+  # Read all json files from directory
+  def self.tag_files
+    return Dir['/etc/tags/*.json']
   end
+
+  def self.tags
+    # loop through through all those files
+    values = Tags.tag_files.map do |f|
+      file = File.read(f)
+      # Quick check for valid JSON
+      if Tags.valid_json?(file)
+        JSON.parse(file)
+      end
+    end
+  end
+
+  def self.final_tags
+    clean_tags = self.tags.flatten!
+    clean_tags.compact!
+    clean_tags.uniq!
+    return clean_tags
+  end
+
+  def self.add_facts
+    Facter.add(:tags) do
+      confine :kernel => 'Linux'
+      setcode do
+        Tags.final_tags unless Tags.final_tags.empty?
+      end
+    end
+  end
+
 end
+
+# Add the facts!
+Tags.add_facts
+
+
